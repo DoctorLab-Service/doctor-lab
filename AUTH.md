b58c82790fe8d40e4a78163b683519b39678ad1a
MaridiM <maridim92@gmail.com>
2022-06-14 21:42:54 +0300

    Set Base params and coinfig app

---
+++ AUTH.md
@@ -0,0 +1,21 @@
+### Auth Service
+
+- [] - Register
+- [] - Register by Facebook
+- [] - Register by Google
+- [] - Login
+
+### Verification Service
+
+- [] - Verification Email Entity
+- [] - Verification Phone Entity
+- [] - Verification Email
+- [] - Verification Phone
+
+### Reset Service
+
+- [] - Reset Password
+- [] - Confirm Password
+- [] - Reset Change Password
+- [] - Reset Email
+- [] - Reset Phone
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,53 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ResetModule } from './reset/reset.module'
+import { VerificationModule } from './verification/verification.module'
+import { AuthModule } from './auth/auth.module'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { AppResolvers } from './app.resolver'
+import { Module } from '@nestjs/common'
+import * as Joi from 'joi'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: process.env.NODE_ENV === 'development' ? '.env.development' : '.env.test',
+            ignoreEnvFile: process.env.NODE_ENV === 'production',
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().valid('development', 'production').required(),
+                PORT: Joi.number().required(),
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.number().required(),
+                DB_USERNAME: Joi.string().required(),
+                DB_PASSWORDS: Joi.string().required(),
+                DB_NAME: Joi.string().required(),
+                PRIVATE_KEY: Joi.string().required(),
+                SENDGRID_API_KEY: Joi.string().required(),
+                SENDGRID_EMAIL: Joi.string().required(),
+            }),
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORDS,
+            database: process.env.DB_NAME,
+            synchronize: process.env.NODE_ENV !== 'production',
+            logging: process.env.NODE_ENV !== 'production',
+            entities: [],
+        }),
+        GraphQLModule.forRoot<ApolloDriverConfig>({
+            driver: ApolloDriver,
+            path: 'auth',
+            autoSchemaFile: true,
+        }),
+        AuthModule,
+        VerificationModule,
+        ResetModule,
+    ],
+    providers: [AppResolvers],
 })
 export class AppModule {}

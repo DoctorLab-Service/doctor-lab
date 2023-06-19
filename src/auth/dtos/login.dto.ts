0bdd916a3794b1280e1ada201f457f4474d134fd
maridim <maridim.dev@gmail.com>
2023-06-19 12:31:54 +0300

    Add  role in input ligin

--- src/auth/dtos/login.dto.ts
+++ src/auth/dtos/login.dto.ts
@@ -1,5 +1,6 @@
 import { Field, InputType, IntersectionType, ObjectType, PartialType, PickType } from '@nestjs/graphql'
 import { CoreOutput } from 'src/common/dtos/output.dto'
+import { EDefaultRoles } from 'src/roles/roles.enums'
 import { User } from 'src/users/entities'
 
 @InputType()
@@ -9,7 +10,10 @@ export class NoRequiredInput extends PartialType(PickType(User, ['phone', 'email
 export class RequiredInput extends PickType(User, ['password']) {}
 
 @InputType()
-export class LoginInput extends IntersectionType(RequiredInput, NoRequiredInput) {}
+export class LoginInput extends IntersectionType(RequiredInput, NoRequiredInput) {
+    @Field(() => EDefaultRoles)
+    role: EDefaultRoles
+}
 
 @ObjectType()
 export class LoginOutput extends CoreOutput {

9353bb908e322abcde6373c4072840954182bc51
MaridiM <maridim92@gmail.com>
2024-05-16 21:46:12 +0300

    Add vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+    }
+};
 
 export default nextConfig;
